991d666375f6243a8335dfdc248683f728171c88
Leo Gastelum <16061298+drkbal0@users.noreply.github.com>
2025-12-24 19:23:48 -0600

    priv
    xd

--- config.php
+++ config.php
@@ -4,11 +4,12 @@
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 session_start();
-$host = 'sql108.byetcluster.com'; // Tu MySQL hostname
-$user = 'mseet_40694072'; // Tu FTP/MySQL username
-$pass = 'UjO1qflpDJmy';    // Tu contraseña de hosting
-$db   = 'mseet_40694072_links'; // Nombre de la DB creada
+$host = ''; // Tu MySQL hostname
+$user = ''; // Tu FTP/MySQL username
+$pass =     // Tu contraseña de hosting
+$db   =  // Nombre de la DB creada
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) die("Error de conexión");
+
 ?>
